fc820a21673f53fdc716deaeb95fb67f53f7de69
VanceKingSaxbeA <vsaxbe@yahoo.com>
2014-02-23 23:38:34 +0530

    16409 th/nd times commit by Vance King Saxbe. A and development team Power Dominion Enterprise

--- src/yahooquotemachine.py
+++ src/yahooquotemachine.py
@@ -1,4 +1,4 @@
-from src.yahoofinancequote import *
+""" Copyright (c) <2014> Author Vance King Saxbe. A, and contributors Power Dominion Enterprise, Precieux Consulting and other contributors. Modelled, Architected and designed by Vance King Saxbe. A. with the geeks from GoldSax Consulting and GoldSax Technologies email @vsaxbe@yahoo.com. Development teams from Power Dominion Enterprise, Precieux Consulting. Project sponsored by GoldSax Foundation, GoldSax Group and executed by GoldSax Manager."""from src.yahoofinancequote import *
 import sqlite3 as lite
 import string
 import gc
